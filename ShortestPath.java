408822d4cfcf34558092f024f5e31d318ff0f9a2
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-17 10:51:25 +0530

    Implement shortest path calculation from directional moves

---
+++ ShortestPath.java
@@ -0,0 +1,31 @@
+public class ShortestPath {
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+
+        int x = 0, y = 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // South
+            if (dir == 'S') {
+                y--;
+                // North
+            } else if (dir == 'N') {
+                y++;
+                // West
+            } else if (dir == 'W') {
+                x--;
+                // East
+            } else {
+                x++;
+            }
+
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+
+        float ans = (float) Math.sqrt(X2 + Y2);
+
+        System.out.println(ans);
+    }
+}
