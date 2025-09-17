f91143e0787e084f8a62e12b989682ccf2bbc2c1
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-17 09:45:38 +0530

    Palindrome LeetCode 125

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "madam";
+
+    }
+}
