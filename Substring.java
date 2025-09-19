1663af75150928fbbf49cc3b07763eafd3a94a65
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-19 10:51:47 +0530

    Add example of extracting substring from a string

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args) {
+        String s1 = "Tony Stark";
+        String s2 = s1.substring(0, 4);
+        System.out.println(s2);
+    }
+
+}
