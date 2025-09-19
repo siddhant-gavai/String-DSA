1a6753a3bbb567532ff07af69fd5e9e41bcae0e1
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-19 10:56:33 +0530

    Implement manual substring extraction using character iteration

--- Substring.java
+++ Substring.java
@@ -1,8 +1,16 @@
 public class Substring {
     public static void main(String[] args) {
         String s1 = "Tony Stark";
-        String s2 = s1.substring(0, 4);
-        System.out.println(s2);
+        // String s2 = s1.substring(0, 4);
+        // System.out.println(s2);
+
+        String subString = "";
+        int si = 0;
+        int ei = 8;
+        for (int i = si; i < ei; i++) {
+            subString += s1.charAt(i);
+        }
+        System.out.println(subString);
     }
 
 }
